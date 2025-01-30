9ab7bfb621c659404bb7c1b5e6f50420d543c8aa
nikolalazarevic011 <nikola.lazaa@gmail.com>
2025-01-29 19:51:11 -0600

    material ui added

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider, Button } from "@rneui/themed";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <ThemeProvider>
+            <View style={styles.container}>
+                <Text>Hello React Native Elements!</Text>
+                <Button
+                    title="Press Me"
+                    onPress={() => alert("Button Pressed!")}
+                />
+            </View>
+        </ThemeProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
