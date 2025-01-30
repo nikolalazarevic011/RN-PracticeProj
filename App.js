ed9beec76406fc2706ddf4c80aa644977b149caa
nikolaLazarevic95 <nikola.lazaa@gmail.com>
2025-01-30 09:18:43 -0600

    added nav config

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import AllMovies from "./screens/AllMovies";
 import Watchlist from "./screens/Watchlist";
 import MovieDetail from "./screens/MovieDetail";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function App() {
     const Stack = createNativeStackNavigator();
@@ -15,9 +16,48 @@ export default function App() {
 
     function MoviesOverview() {
         return (
-            <BottomTabs.Navigator>
-                <BottomTabs.Screen name="AllMovies" component={AllMovies} />
-                <BottomTabs.Screen name="Watchlist" component={Watchlist} />
+            <BottomTabs.Navigator
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: theme.lightColors.secondary,
+                    },
+                    headerTintColor: "white",
+                    tabBarStyle: {
+                        backgroundColor: theme.lightColors.secondary,
+                    },
+                    tabBarActiveTintColor: theme.darkColors.secondary,
+                }}
+            >
+                <BottomTabs.Screen
+                    name="AllMovies"
+                    component={AllMovies}
+                    options={{
+                        title: "All Movies",
+                        tabBarLabel: "All Movies",
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="home-outline"
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+                <BottomTabs.Screen
+                    name="Watchlist"
+                    component={Watchlist}
+                    options={{
+                        title: "Watchlist",
+                        tabBarLabel: "Watchlist",
+                        tabBarIcon: ({ color, size }) => (
+                            <Ionicons
+                                name="bookmark-outline"
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
             </BottomTabs.Navigator>
         );
     }
@@ -28,6 +68,7 @@ export default function App() {
                     <Stack.Screen
                         name="MoviesOverview"
                         component={MoviesOverview}
+                        options={{ headerShown: false }}
                     />
                     <Stack.Screen name="AllMovies" component={MovieDetail} />
                 </Stack.Navigator>
